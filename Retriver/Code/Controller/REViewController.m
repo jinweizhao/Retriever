04f8394c7467afb4538322aecef7d944d1339621
钟颖 <cyan.zy@alibaba-inc.com>
2016-10-21 14:57:39 +0800

    Fix Typo

--- Retriver/Code/Controller/REViewController.m
+++ Retriver/Code/Controller/REViewController.m
@@ -58,7 +58,7 @@
     id app = self.apps[indexPath.row];
     cell.imageView.image = [UIImage invoke:@"_applicationIconImageForBundleIdentifier:format:scale:"
                                  arguments:@[[app valueForKey:@"bundleIdentifier"], @(10), @([UIScreen mainScreen].scale)]];
-    cell.imageView.transform = CGAffineTransformScale(CGAffineTransformIdentity, 0.5, 0.);
+    cell.imageView.transform = CGAffineTransformScale(CGAffineTransformIdentity, 0.5, 0.5);
     cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
     cell.textLabel.text = [self displayNameAtIndexPath:indexPath];
     return cell;
