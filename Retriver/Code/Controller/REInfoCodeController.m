33ab4ab0995e9bf9ac822d4f4128f72f1ef3efc4
钟颖 <cyan.zy@alibaba-inc.com>
2016-10-24 14:55:08 +0800

    Lazy Initialize

--- Retriver/Code/Controller/REInfoCodeController.m
+++ Retriver/Code/Controller/REInfoCodeController.m
@@ -19,7 +19,8 @@ typedef NS_ENUM(NSInteger, REInfoCodeType) {
 
 @property (nonatomic, readonly) UISegmentedControl *segmentedControl;
 @property (nonatomic, strong) id info;
-@property (nonatomic, copy) NSString *code;
+@property (nonatomic, copy) NSString *json;
+@property (nonatomic, copy) NSString *xml;
 @property (nonatomic, strong) WKWebView *webView;
 @property (nonatomic, strong) JSValue *parser;
 
@@ -32,18 +33,6 @@ typedef NS_ENUM(NSInteger, REInfoCodeType) {
         _info = info;
     }
     return self;
-}
-
-- (JSValue *)parser {
-    if (_parser == nil) {
-        JSContext *context = [[JSContext alloc] init];
-        NSString *script = [NSString stringWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"vkbeautify" ofType:@"js"]
-                                                     encoding:NSUTF8StringEncoding
-                                                        error:nil];
-        [context evaluateScript:script];
-        _parser = context[@"parser"][@"xml"];
-    }
-    return _parser;
 }
 
 - (void)done:(UIBarButtonItem *)sender {
@@ -97,13 +86,9 @@ typedef NS_ENUM(NSInteger, REInfoCodeType) {
 
 - (void)refresh {
     dispatch_async(dispatch_get_global_queue(0, 0), ^{
-        switch (self.segmentedControl.selectedSegmentIndex) {
-            case REInfoCodeTypeJSON: [self convertToJSON]; break;
-            case REInfoCodeTypeXML: [self convertToXML]; break;
-            default: break;
-        }
+        NSString *code = self.segmentedControl.selectedSegmentIndex == REInfoCodeTypeJSON ? self.json : self.xml;
         dispatch_async(dispatch_get_main_queue(), ^{
-            [self.webView loadHTMLString:self.code baseURL:[[NSBundle mainBundle] bundleURL]];
+            [self.webView loadHTMLString:code baseURL:[[NSBundle mainBundle] bundleURL]];
         });
     });
 }
@@ -118,25 +103,45 @@ typedef NS_ENUM(NSInteger, REInfoCodeType) {
     return [NSString stringWithFormat:format, type, code];
 }
 
-- (void)convertToJSON {
-    NSData *data = [NSJSONSerialization dataWithJSONObject:self.info
-                                                   options:NSJSONWritingPrettyPrinted
-                                                     error:nil];
-    NSString *json = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
-    self.code = [self wrapCode:json type:@"json"];
+#pragma mark - Lazy Init
+
+- (NSString *)json {
+    if (_json == nil) {
+        NSData *data = [NSJSONSerialization dataWithJSONObject:self.info
+                                                       options:NSJSONWritingPrettyPrinted
+                                                         error:nil];
+        NSString *json = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+        _json = [self wrapCode:json type:@"json"];
+    }
+    return _json;
 }
 
-- (void)convertToXML {
-    NSData *data = [NSPropertyListSerialization dataWithPropertyList:self.info
-                                                              format:NSPropertyListXMLFormat_v1_0
-                                                             options:0
-                                                               error:nil];
-    NSArray *arguments = @[[[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding], @(2)];
-    NSString *pretty = [[self.parser callWithArguments:arguments] toString];
-    NSMutableString *xml = pretty.mutableCopy;
-    [xml replaceOccurrencesOfString:@"<" withString:@"&lt;" options:0 range:NSMakeRange(0, xml.length)];
-    [xml replaceOccurrencesOfString:@">" withString:@"&gt;" options:0 range:NSMakeRange(0, xml.length)];
-    self.code = [self wrapCode:xml type:@"xml"];
+- (NSString *)xml {
+    if (_xml == nil) {
+        NSData *data = [NSPropertyListSerialization dataWithPropertyList:self.info
+                                                                  format:NSPropertyListXMLFormat_v1_0
+                                                                 options:0
+                                                                   error:nil];
+        NSArray *arguments = @[[[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding], @(2)];
+        NSString *pretty = [[self.parser callWithArguments:arguments] toString];
+        NSMutableString *xml = pretty.mutableCopy;
+        [xml replaceOccurrencesOfString:@"<" withString:@"&lt;" options:0 range:NSMakeRange(0, xml.length)];
+        [xml replaceOccurrencesOfString:@">" withString:@"&gt;" options:0 range:NSMakeRange(0, xml.length)];
+        _xml = [self wrapCode:xml type:@"xml"];
+    }
+    return _xml;
+}
+
+- (JSValue *)parser {
+    if (_parser == nil) {
+        JSContext *context = [[JSContext alloc] init];
+        NSString *script = [NSString stringWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"vkbeautify" ofType:@"js"]
+                                                     encoding:NSUTF8StringEncoding
+                                                        error:nil];
+        [context evaluateScript:script];
+        _parser = context[@"parser"][@"xml"];
+    }
+    return _parser;
 }
 
 @end
